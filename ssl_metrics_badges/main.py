29c6e5fb1a97dfe10049701b6c8874e184af487c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-28 20:05:46 -0500

    Add output arg and check for correct file extension

--- ssl_metrics_badges/main.py
+++ ssl_metrics_badges/main.py
@@ -22,5 +22,22 @@ def getArgs() -> Namespace:
         type=str,
         help="The SVG file to be converted into a badge",
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        required=True,
+        type=str,
+        help="The output filename of the badge. NOTE: Must end in .svg",
+    )
 
     return parser.parse_args()
+
+def main()  ->  None:
+    args: Namespace = getArgs()
+
+    if args.output[-4::] != ".svg":
+        print("Invalid output file extension. File must end in .svg")
+        quit(1)
+
+if __name__ == "__main__":
+    main()
