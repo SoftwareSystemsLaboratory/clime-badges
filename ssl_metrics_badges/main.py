793c4f44c729658d8139e77e6d5741be0b9d6d31
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-28 19:59:58 -0500

    Setup project

--- ssl_metrics_badges/main.py
+++ ssl_metrics_badges/main.py
@@ -1,5 +1,10 @@
 from pybadges import badge
 
-temp:str = badge(left_text="Hello World", right_text="This is a test", left_color="blue", right_color="red")
+temp: str = badge(
+    left_text="Hello World",
+    right_text="This is a test",
+    left_color="blue",
+    right_color="red",
+)
 
 print(temp)
